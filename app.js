40c8659b888f6fc0d0011d386677ae425e93b51c
Şüheda <92635912+SuhedaTaspinar@users.noreply.github.com>
2024-09-14 15:08:09 +0300

    `

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-
 const path = require('path');
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// CORS Middleware'i
+// CORS Middleware
 app.use(cors());
 
 // Connect to database
@@ -18,10 +17,9 @@ app.use(express.json()); // Parse JSON bodies
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use("/api/projects", require('./routes/projectRoutes'));
-app.use("/api/contacts", require('./routes/contactRoutes'));
-app.use("/api/services", require('./routes/serviceRoutes'));
+app.use('/api/projects', require('./routes/projectRoutes'));
+app.use('/api/contacts', require('./routes/contactRoutes'));
+app.use('/api/services', require('./routes/serviceRoutes'));
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel to handle as an API route
+module.exports = app;
